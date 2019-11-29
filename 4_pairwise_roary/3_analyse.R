c7c30a32e8fc75b6fd708f7ba4f7d1d98a9854ef
Gal Horesh <gh11@sanger.ac.uk>
2019-11-29 15:39:19 +0000

    updates to pairwise roary plotting script

--- 4_pairwise_roary/3_analyse.R
+++ 4_pairwise_roary/3_analyse.R
@@ -21,40 +21,71 @@ freqs = freqs[,-which(colnames(freqs) == "X50")]
 clusters = sapply(X = colnames(freqs), FUN = gsub, pattern = "X", replacement = "")
 graphics = graphics[match(clusters, graphics$Cluster),]
 o = clusters
+phylo_order = c("B1","A","C","E","D","B2","F","U")
 
-for_pca = t(freqs)
-remove = c()
-for (i in 1:dim(for_pca)[2]) {
-  if (length(unique(for_pca[,i])) == 1) { ## no variation in gene
-    remove = c(remove, i)
+create_pca_plot <- function(curr_freqs){
+  for_pca = t(curr_freqs)
+  remove = c()
+  for (i in 1:dim(for_pca)[2]) {
+    if (length(unique(for_pca[,i])) == 1) { ## no variation in gene
+      remove = c(remove, i)
+    }
   }
+  for_pca = for_pca[,-remove]
+  ### PCA plot of the clusters -> what are the relationships between the clusters based on the frequencies of all genes
+  freqs.pca = prcomp(for_pca , center = T)
+  summary(freqs.pca)
+  freqs.pca = data.frame(freqs.pca$x)
+  freqs.pca = cbind(freqs.pca, Cluster = as.character(o))
+  freqs.pca$Cluster = factor(freqs.pca$Cluster, o)
+  return (freqs.pca)
 }
-for_pca = for_pca[,-remove]
 
+freqs.pca = create_pca_plot(freqs)
 ## to focus only on particular gene
 # counts = apply(FUN = function(v) length(which(v < 0.15)), 2, X = for_pca)
 # for_pca = for_pca[,which(counts==47)]
 
-### PCA plot of the clusters -> what are the relationships between the clusters based on the frequencies of all genes
-freqs.pca = prcomp(for_pca , center = T)
-summary(freqs.pca)
-freqs.pca = data.frame(freqs.pca$x)
-freqs.pca = cbind(freqs.pca, Cluster = as.character(o))
-freqs.pca$Cluster = factor(freqs.pca$Cluster, o)
-
 
 ## PCA plot
-B=ggplot(freqs.pca, aes(x = PC1, y = PC2, color = Cluster, shape = Cluster)) + geom_point(size = 3.5, stroke = 1, alpha = 0.7) +
+C = ggplot(freqs.pca, aes(x = PC1, y = PC2, color = Cluster, shape = Cluster)) + geom_point(size = 3.5, stroke = 1, alpha = 0.7) +
   scale_color_manual(values = graphics$Color, guide = F) + theme_bw(base_size = 12) +
   geom_text(aes(label=Cluster),hjust=-0.3, vjust=-0.3) +
   scale_shape_manual(values =  graphics$Shape, guide = F) + ggtitle("C")  +
-   annotate("text", x = -12, y = 1, label = "B1", size = 5, parse = T) +
-     annotate("text", x = 0.16, y = 0.02 , label = "A", size = 5, parse = T) + 
-   annotate("text", x = -8, y = -8 , label = "E", size = 5,parse = T)+ 
-   annotate("text", x = 8, y = -11 , label = "F", size = 5,parse = T)+ 
+  annotate("text", x = -12, y = 1, label = "B1", size = 5, parse = T) +
+  annotate("text", x = 0.16, y = 0.02 , label = "A", size = 5, parse = T) + 
+  annotate("text", x = -8, y = -8 , label = "E", size = 5,parse = T)+ 
+  annotate("text", x = 8, y = -11 , label = "F", size = 5,parse = T)+ 
   annotate("text", x = -3, y = -13 , label = "D", size = 5,parse = T)+ 
-   annotate("text", x = 10, y = 4 , label = "B2", size = 5,parse = T)
-B
+  annotate("text", x = 10, y = 4 , label = "B2", size = 5,parse = T)
+C
+
+# ## create PCA plots for genes which are core (i.e. core in 47 of 47) -> plotting according to core is meaningless because it's the same for all...
+# keep = c()
+# for (i in 1:dim(freqs)[1]) {
+#   non_inter = length(which(freqs[i,] < 0.15 | freqs[i,] >= 0.9))
+#   if (non_inter == 0) { keep = c(keep, i)}
+# }
+# curr_freqs = freqs[keep,]
+# freqs.core.pca = create_pca_plot(curr_freqs)
+# ggplot(freqs.core.pca, aes(x = PC1, y = PC2, color = Cluster, shape = Cluster)) + geom_point(size = 3.5, stroke = 1, alpha = 0.7) +
+#   scale_color_manual(values = graphics$Color, guide = F) + theme_bw(base_size = 12) +
+#   geom_text(aes(label=Cluster),hjust=-0.3, vjust=-0.3) +
+#   scale_shape_manual(values =  graphics$Shape, guide = F) + ggtitle("C")
+# 
+# ## create PCA plots for genes which are rare (always under 15%)
+# keep = c()
+# for (i in 1:dim(freqs)[1]) {
+#   non_rare = length(which(freqs[i,] > 0.15))
+#   if (non_rare == 0) { keep = c(keep, i)}
+# }
+# curr_freqs = freqs[keep,]
+# freqs.rare.pca = create_pca_plot(curr_freqs)
+# ggplot(freqs.rare.pca, aes(x = PC4, y = PC3, color = Cluster, shape = Cluster)) + geom_point(size = 3.5, stroke = 1, alpha = 0.7) +
+#   scale_color_manual(values = graphics$Color, guide = F) + theme_bw(base_size = 12) +
+#   geom_text(aes(label=Cluster),hjust=-0.3, vjust=-0.3) +
+#   scale_shape_manual(values =  graphics$Shape, guide = F) + ggtitle("C")
+# 
 
 
 # 
@@ -82,23 +113,32 @@ curr = data.frame(name = o,
                   freq = unlist(freqs[which(rownames(freqs) == gene_name),]), stringsAsFactors = F)
 curr = cbind(curr, phylo = graphics$Phylogroup[match(curr$name, graphics$Cluster)])
 curr$name = factor(curr$name, o)
-intA = ggplot(curr, aes(x = name, y = freq)) + geom_bar(stat = "identity") +
-  scale_y_continuous(limits = c(0,1), expand = c(0,0)) + theme_bw(base_size = 12) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+curr$phylo = factor(curr$phylo, phylo_order)
+A = ggplot(curr, aes(x = name, y = freq)) + geom_bar(stat = "identity") +
+  scale_y_continuous(limits = c(0,1), expand = c(0,0)) + theme_bw(base_size = 12) + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   ylab("Frequency") + xlab("PopPUNK cluster") + 
   facet_grid(~phylo, scales = "free",  space = "free",switch = "x")  +
   ggtitle("A - intA")
-intA
+
 
 gene_name = "wzyE" ## change here to check the frequency of a gene across the popPUNK clusters
 curr = data.frame(name = o,
                   freq = unlist(freqs[which(rownames(freqs) == gene_name),]), stringsAsFactors = F)
 curr = cbind(curr, phylo = graphics$Phylogroup[match(curr$name, graphics$Cluster)])
 curr$name = factor(curr$name, o)
-wzyE = ggplot(curr, aes(x = name, y = freq)) + geom_bar(stat = "identity") +
+curr$phylo = factor(curr$phylo, phylo_order)
+B = ggplot(curr, aes(x = name, y = freq)) + geom_bar(stat = "identity") +
   scale_y_continuous(limits = c(0,1), expand = c(0,0)) + theme_bw(base_size = 12) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   ylab("Frequency") + xlab("PopPUNK cluster") + 
   facet_grid(~phylo, scales = "free",  space = "free",switch = "x")  +
-  ggtitle("C - wzyE")
+  ggtitle("B - wzyE")
+
+
+grid.arrange(A, B, C, layout_matrix = rbind(c(1),
+                                            c(2),
+                                            c(3),
+                                            c(3)))
 
 
 ## plot presence and absence of two genes -> i.e. which clusters have one gene and not another - on a tree
@@ -129,8 +169,8 @@ rownames(res) = res[,1]
 res = res[,-1]
 
 gheatmap(p = p, data = res, offset = 0.1, width=5, color = "black", 
-             high = "#023858", low = "#fff7fb", colnames_angle = 90, colnames = T, colnames_position = "top",
-             font.size = 3, colnames_offset_y = 5)
+         high = "#023858", low = "#fff7fb", colnames_angle = 90, colnames = T, colnames_position = "top",
+         font.size = 3, colnames_offset_y = 5)
 ggplot(curr,aes(x = name, y = gene, fill = freq)) + geom_tile(stat = "identity", color = "black") + 
   theme_classic(base_size = 14) + scale_fill_gradient(low = "white", high = "black") +
   scale_y_discrete(labels = c("effector","hypothetical\nprotein","immunity")) +
@@ -194,10 +234,10 @@ p1
 # p1 = p1 + theme(legend.position = "None")
 # p1
 grid.arrange(intA,wzyE, p1, B, layout_matrix = rbind(c(1,1,1,2,2,2),
-                                                    c(1,1,1,2,2,2),
-                                                    c(4,4,3,3,3,3),
-                                                    c(4,4,3,3,3,3),
-                                                    c(4,4,3,3,3,3)))
+                                                     c(1,1,1,2,2,2),
+                                                     c(4,4,3,3,3,3),
+                                                     c(4,4,3,3,3,3),
+                                                     c(4,4,3,3,3,3)))
 #write.table(x = gene_classes, file  = "231019_corrected///gene_classes.csv", sep = ",", col.names = T, row.names = T,quote = F)
 
 ### ubiquitous genes
